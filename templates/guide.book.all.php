950c220298cdc975a30375788a70b2532433c085
eric mcgregor <eric.mcgregor@rackspace.com>
2013-09-13 12:40:32 -0500

    added tool to book/all

--- templates/guide.book.all.php
+++ templates/guide.book.all.php
@@ -83,14 +83,6 @@
 					<div class="col-md-11 col-md-offset-1">
 						{{chapter.content|raw}}
 
-						<div class="actionOverview hide">
-							<ul>
-								{% for code in chapter.code %}
-									<li><pre class="{{code.type}}">{{code.text}}</pre></li>
-								{%endfor%}
-							</ul>
-						
-						</div>
 					</div>
 
 					
@@ -98,6 +90,32 @@
 				<hr>
 
 			</div>
+
+
+			{% for code in chapter.code %}
+			<div class="container">
+				<div class="row actionOverview">
+					<div class="sidebar col-md-4">
+						{% if code.iptool %}
+						    {% include 'partials/ip-tool.php' %}
+						{% else %}
+						
+						{% endif %}
+						
+					</div>
+					<div class="col-md-11 col-md-offset-1">
+						<div class="">
+							<ul>
+								<li><pre class="{{code.type}}">{{code.text}}</pre></li>
+							</ul>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{%endfor%}
+
 				<!-- PAGE SECTION -->
 			{% for page in chapter.children %}
 			<div id="{{page.slug}}" class="page-container" ng-repeat="page in chapter.children">
