af40a8b85a261075b5cd0fc37817751947c4dce3
jonstovall <jonathan@jonathanstovall.com>
2014-02-19 14:32:17 -0600

    fix "imformative"

--- templates/partials/action-options.php
+++ templates/partials/action-options.php
@@ -8,7 +8,7 @@
 		<a href="{{ baseurl }}/guides/{{guide.slug}}/{{book.slug}}/{{chapter.previous.slug}}" class="btn btn-default tooltip-rs" title="Previous chapter"><span class="glyphicon glyphicon-chevron-left"></span></a>
 	{% endif %}
 	
-	<button type="button" data-toggle-expert class="btn btn-default tooltip-rs" title="Toggle imformative content" onClick="_gaq.push(['_trackEvent', 'Buttons', 'Click', 'Expert Mode’]);"><span class="glyphicon glyphicon-certificate"></span>Expert Mode</button>
+	<button type="button" data-toggle-expert class="btn btn-default tooltip-rs" title="Toggle informative content" onClick="_gaq.push(['_trackEvent', 'Buttons', 'Click', 'Expert Mode’]);"><span class="glyphicon glyphicon-certificate"></span>Expert Mode</button>
 	<button type="button" data-toggle-nav class="btn btn-default" >Guide Index<span class="caret"></span></button>
 	{% if allsteps %}
 	<!-- {% if book.next %}
